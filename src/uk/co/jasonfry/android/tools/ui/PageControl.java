5bd08b756a30d344d07ed9ab995037cf80a8eaec
Jason Fry <jason@jasonfry.co.uk>
2010-08-18 13:50:45 +0100

    First upload

---
+++ src/uk/co/jasonfry/android/tools/ui/PageControl.java
@@ -0,0 +1,336 @@
+/*
+ * Copyright (C) 2010 Jason Fry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jason Fry - jasonfry.co.uk
+ * @version 1.0
+ * 
+ */
+
+package uk.co.jasonfry.android.tools.ui;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.graphics.drawable.shapes.Shape;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class PageControl extends LinearLayout
+{
+	private int mIndicatorSize = 7;
+	
+	private Drawable activeDrawable;
+	private Drawable inactiveDrawable;
+	
+	private ArrayList<ImageView> indicators;
+	
+	private int mPageCount = 0;
+	private int mCurrentPage = 0;
+	
+	private Context mContext;
+	private OnPageControlClickListener mOnPageControlClickListener = null;
+	
+	public PageControl(Context context) 
+	{
+		super(context);
+		mContext = context;
+		initPageControl();
+	}
+	
+	public PageControl(Context context, AttributeSet attrs) 
+	{
+		super(context, attrs);
+		mContext = context;
+		//will now wait until onFinishInflate to call initPageControl()
+	}
+	
+	@Override
+	protected void onFinishInflate()
+	{
+		initPageControl();
+	}
+	
+	private void initPageControl()
+	{
+		Log.i("uk.co.jasonfry.android.tools.ui.PageControl","Initialising PageControl");
+		
+		indicators = new ArrayList<ImageView>();
+		
+		activeDrawable = new ShapeDrawable();
+		inactiveDrawable = new ShapeDrawable();
+		
+		activeDrawable.setBounds(0, 0, mIndicatorSize, mIndicatorSize);
+		inactiveDrawable.setBounds(0, 0, mIndicatorSize, mIndicatorSize);
+		
+		Shape s1 = new OvalShape();
+		s1.resize(mIndicatorSize, mIndicatorSize);
+		
+		Shape s2 = new OvalShape();
+		s2.resize(mIndicatorSize, mIndicatorSize);
+		
+		int i[] = new int[2];
+		i[0] = android.R.attr.textColorSecondary;
+		i[1] = android.R.attr.textColorSecondaryInverse;
+		TypedArray a = mContext.getTheme().obtainStyledAttributes(i);
+
+		((ShapeDrawable) activeDrawable).getPaint().setColor(a.getColor(0, Color.DKGRAY));
+		((ShapeDrawable) inactiveDrawable).getPaint().setColor(a.getColor(1, Color.LTGRAY));
+
+		((ShapeDrawable) activeDrawable).setShape(s1);
+		((ShapeDrawable) inactiveDrawable).setShape(s2);
+		
+		mIndicatorSize = (int) (mIndicatorSize * getResources().getDisplayMetrics().density);
+		
+		setOnTouchListener(new OnTouchListener() 
+		{	
+			public boolean onTouch(View v, MotionEvent event) 
+			{
+				if(mOnPageControlClickListener != null)
+				{
+					switch(event.getAction())
+					{
+						case MotionEvent.ACTION_UP :
+							
+							if(PageControl.this.getOrientation() == LinearLayout.HORIZONTAL)
+							{
+								if(event.getX()<(PageControl.this.getWidth()/2)) //if on left of view
+								{
+									if(mCurrentPage>0)
+									{
+										mOnPageControlClickListener.goBackwards();
+									}
+								}
+								else //if on right of view
+								{
+									if(mCurrentPage<(mPageCount-1))
+									{
+										mOnPageControlClickListener.goForwards();
+									}
+								}
+							}
+							else
+							{
+								if(event.getY()<(PageControl.this.getHeight()/2)) //if on top half of view
+								{
+									if(mCurrentPage>0)
+									{
+										mOnPageControlClickListener.goBackwards();
+									}
+								}
+								else //if on bottom half of view
+								{
+									if(mCurrentPage<(mPageCount-1))
+									{
+										mOnPageControlClickListener.goForwards();
+									}
+								}
+							}
+							
+							
+							return false;
+					}
+				}
+				return true;
+			}
+		});
+	}
+	
+	/**
+	 * Set the drawable object for an active page indicator
+	 * 
+	 * @param d The drawable object for an active page indicator
+	 */
+	public void setActiveDrawable(Drawable d)
+	{
+		activeDrawable = d;
+		
+		indicators.get(mCurrentPage).setBackgroundDrawable(activeDrawable);
+		
+	}
+	
+	/**
+	 *  Return the current drawable object for an active page indicator
+	 * 
+	 * @return Returns the current drawable object for an active page indicator
+	 */
+	public Drawable getActiveDrawable()
+	{
+		return activeDrawable;
+	}
+	
+	/**
+	 *  Set the drawable object for an inactive page indicator
+	 * 
+	 * @param d The drawable object for an inactive page indicator
+	 */
+	public void setInactiveDrawable(Drawable d)
+	{
+		inactiveDrawable = d;
+		
+		for(int i=0; i<mPageCount; i++)
+		{
+			indicators.get(i).setBackgroundDrawable(inactiveDrawable);
+		}
+		
+		indicators.get(mCurrentPage).setBackgroundDrawable(activeDrawable);
+	}
+
+	/**
+	 * Return the current drawable object for an inactive page indicator
+	 * 
+	 * @return Returns the current drawable object for an inactive page indicator
+	 */
+	public Drawable getInactiveDrawable()
+	{
+		return inactiveDrawable;
+	}
+	
+	/**
+	 * Set the number of pages this PageControl should have
+	 * 
+	 * @param pageCount The number of pages this PageControl should have
+	 */
+	public void setPageCount(int pageCount)
+	{
+		mPageCount = pageCount;
+		for(int i=0;i<pageCount;i++)
+		{
+			indicators.add(new ImageView(mContext));
+			indicators.get(i).setLayoutParams(new LayoutParams(mIndicatorSize, mIndicatorSize));
+			indicators.get(i).setBackgroundDrawable(inactiveDrawable);
+
+			FrameLayout f = new FrameLayout(mContext);
+			f.setPadding(mIndicatorSize/2, mIndicatorSize, mIndicatorSize/2, mIndicatorSize);
+			f.addView(indicators.get(i));
+			
+			addView(f);
+		}
+	}
+	
+	/**
+	 * Return the number of pages this PageControl has
+	 * 
+	 * @return Returns the number of pages this PageControl has
+	 */
+	public int getPageCount()
+	{
+		return mPageCount;
+	}
+	
+	/**
+	 * Set the current page the PageControl should be on
+	 * 
+	 * @param currentPage The current page the PageControl should be on
+	 */
+	public void setCurrentPage(int currentPage)
+	{
+		if(currentPage<mPageCount)
+		{
+			indicators.get(mCurrentPage).setBackgroundDrawable(inactiveDrawable);//reset old indicator
+			indicators.get(currentPage).setBackgroundDrawable(activeDrawable);//set up new indicator
+			mCurrentPage = currentPage;
+		}
+	}
+	
+	/**
+	 * Return the current page the PageControl is on
+	 * 
+	 * @return Returns the current page the PageControl is on
+	 */
+	public int getCurrentPage()
+	{
+		return mCurrentPage;
+	}
+	
+	/**
+	 * Set the size of the page indicator drawables
+	 * 
+	 * @param indicatorSize The size of the page indicator drawables
+	 */
+	public void setIndicatorSize(int indicatorSize)
+	{
+		mIndicatorSize=indicatorSize;
+		for(int i=0;i<mPageCount;i++)
+		{
+			indicators.get(i).setLayoutParams(new LayoutParams(mIndicatorSize, mIndicatorSize));
+		}
+	}
+	
+	/**
+	 * Return the size of the page indicator drawables
+	 * 
+	 * @return Returns the size of the page indicator drawables
+	 */
+	public int getIndicatorSize()
+	{
+		return mIndicatorSize;
+	}
+	
+	/**
+	 * 
+	 * @author Jason Fry - jasonfry.co.uk
+	 * 
+	 * Interface definition for a callback to be invoked when a PageControl is clicked.
+	 * 
+	 */
+	public interface OnPageControlClickListener
+	{
+		/**
+		 * Called when the PageControl should go forwards
+		 * 
+		 */
+		public abstract void goForwards();
+		
+		/**
+		 * Called when the PageControl should go backwards
+		 * 
+		 */
+		public abstract void goBackwards();
+	}
+	
+	/**
+	 * Set the OnPageControlClickListener object for this PageControl
+	 * 
+	 * @param onPageControlClickListener The OnPageControlClickListener you wish to set
+	 */
+	public void setOnPageControlClickListener(OnPageControlClickListener onPageControlClickListener)
+	{
+		mOnPageControlClickListener = onPageControlClickListener;
+	}
+	
+	/**
+	 * Return the OnPageControlClickListener that has been set on this PageControl
+	 * 
+	 * @return Returns the OnPageControlClickListener that has been set on this PageControl
+	 */
+	public OnPageControlClickListener getOnPageControlClickListener()
+	{
+		return mOnPageControlClickListener;
+	}
+		
+	
+}
---
+++ src/uk/co/jasonfry/android/tools/ui/SwipeView.java
@@ -0,0 +1,433 @@
+/*
+ * Copyright (C) 2010 Jason Fry
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @author Jason Fry - jasonfry.co.uk
+ * @version 1.0
+ * 
+ */
+
+package uk.co.jasonfry.android.tools.ui;
+
+import uk.co.jasonfry.android.tools.ui.PageControl.OnPageControlClickListener;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+
+
+public class SwipeView extends HorizontalScrollView
+{
+	private LinearLayout mLinearLayout;
+	private Context mContext;
+	private int SCREEN_WIDTH;
+	private int motionStartX;
+	private int distanceX;
+	private int previousDirection;
+	private int mCurrentPage;
+	private boolean firstMotionEvent = true;
+	private OnPageChangedListener mOnPageChangedListener = null;
+	private SwipeOnTouchListener mSwipeOnTouchListener;
+	
+	private PageControl mPageControl = null;
+	
+	private static int DEFAULT_SWIPE_THRESHOLD = 70;
+	
+
+	public SwipeView(Context context) 
+	{
+		super(context);
+		mContext = context;
+		initSwipeView();
+	}
+	
+	public SwipeView(Context context, AttributeSet attrs) 
+	{
+		super(context, attrs);
+		mContext = context;
+		initSwipeView();
+	}
+	
+	public SwipeView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs,defStyle);
+		mContext = context;
+		initSwipeView();
+	}
+	
+	private void initSwipeView()
+	{
+		Log.i("uk.co.jasonfry.android.tools.ui.SwipeView","Initialising SwipeView");
+		mLinearLayout = new LinearLayout(mContext);
+		mLinearLayout.setOrientation(LinearLayout.HORIZONTAL);
+		super.addView(mLinearLayout, -1, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+		
+		setSmoothScrollingEnabled(true);
+		setHorizontalFadingEdgeEnabled(false);
+		setHorizontalScrollBarEnabled(false);
+		
+		Display display = ((WindowManager) mContext.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay(); 
+		SCREEN_WIDTH = (int) (display.getWidth());
+		mCurrentPage = 0;
+		
+		mSwipeOnTouchListener = new SwipeOnTouchListener();
+		setOnTouchListener(mSwipeOnTouchListener);
+		
+		
+	}
+	
+	@Override
+	public boolean onTrackballEvent(MotionEvent event)
+	{
+		return true;
+	}
+	
+	@Override
+	public void addView(View child)
+	{
+		addView(child,-1);
+	}
+	
+	@Override
+	public void addView (View child, int index)
+	{
+		ViewGroup.LayoutParams params;
+		params = new LayoutParams(SCREEN_WIDTH, LayoutParams.FILL_PARENT);
+		addView(child, index, params);
+	}
+	
+	@Override
+	public void addView (View child, ViewGroup.LayoutParams params)
+	{
+		addView (child, -1, new LayoutParams(SCREEN_WIDTH, params.height));
+	}
+	
+	@Override
+	public void addView (View child, int index, ViewGroup.LayoutParams params)
+	{
+		requestLayout();
+		invalidate();
+		mLinearLayout.addView(child, index, params);
+	}
+	
+	/**
+	 * Get the View object that contains all the children of this SwipeView. The same as calling getChildAt(0)
+	 * A SwipeView behaves slightly differently from a normal ViewGroup, all the children of a SwipeView
+	 * sit within a LinearLayout, which then sits within the SwipeView object. 
+	 * 
+	 * @return linearLayout The View object that contains all the children of this view
+	 */
+	public LinearLayout getChildContainer()
+	{
+		return mLinearLayout;
+	}
+	
+	
+	
+	/**
+	 * Get the swiping threshold distance to make the screens change
+	 * 
+	 * @return swipeThreshold The minimum distance the finger should move to allow the screens to change
+	 */
+	public int getSwipeThreshold()
+	{
+		return DEFAULT_SWIPE_THRESHOLD;
+	}
+	
+	/**
+	 * Set the swiping threshold distance to make the screens change
+	 * 
+	 * @param swipeThreshold The minimum distance the finger should move to allow the screens to change
+	 */
+	public void setSwipeThreshold(int swipeThreshold)
+	{
+		DEFAULT_SWIPE_THRESHOLD = swipeThreshold;
+	}
+	
+	/**
+	 * Get the current page the SwipeView is on
+	 * 
+	 * @return The current page the SwipeView is on
+	 */
+	public int getCurrentPage()
+	{
+		return mCurrentPage;
+	}
+	
+	/**
+	 * Return the number of pages in this SwipeView
+	 * 
+	 * @return Returns the number of pages in this SwipeView
+	 */
+	public int getPageCount()
+	{
+		return mLinearLayout.getChildCount();
+	}
+	
+	/**
+	 * Go directly to the specified page
+	 * 
+	 * @param page The page to scroll to
+	 */
+	public void scrollToPage(int page)
+	{
+		scrollTo(page*getMeasuredWidth(),0);
+		if(mOnPageChangedListener!=null)
+		{
+			mOnPageChangedListener.onPageChanged(mCurrentPage, page);
+		}
+		if(mPageControl!=null)
+		{
+			mPageControl.setCurrentPage(page);
+		}
+		mCurrentPage = page;
+	}
+	
+	/**
+	 * Animate a scroll to the specified page
+	 * 
+	 * @param page The page to animate to
+	 */
+	public void smoothScrollToPage(int page)
+	{
+		smoothScrollTo(page*getMeasuredWidth(),0);
+		if(mOnPageChangedListener!=null)
+		{
+			mOnPageChangedListener.onPageChanged(mCurrentPage, page);
+		}
+		if(mPageControl!=null)
+		{
+			mPageControl.setCurrentPage(page);
+		}
+		mCurrentPage = page;
+	}
+	
+	/**
+	 * Assign a PageControl object to this SwipeView. Call after adding all the children
+	 * 
+	 * @param pageControl The PageControl object to assign
+	 */
+	public void setPageControl(PageControl pageControl)
+	{
+		mPageControl = pageControl;
+		
+		pageControl.setPageCount(getPageCount());
+		pageControl.setCurrentPage(mCurrentPage);
+		pageControl.setOnPageControlClickListener(new OnPageControlClickListener() 
+		{
+			
+			public void goForwards() 
+			{
+				smoothScrollToPage(mCurrentPage+1);
+				
+			}
+			
+			public void goBackwards() 
+			{
+				smoothScrollToPage(mCurrentPage-1);
+				
+			}
+		});
+	}
+	
+	/**
+	 * Return the current PageControl object
+	 * 
+	 * @return Returns the current PageControl object
+	 */
+	public PageControl getPageControl()
+	{
+		return mPageControl;
+	}
+	
+	/**
+	 * Implement this listener to listen for page change events
+	 * 
+	 * @author Jason Fry - jasonfry.co.uk
+	 *
+	 */
+	public interface OnPageChangedListener
+	{
+		/**
+		 * Event for when a page changes
+		 * 
+		 * @param oldPage The page the view was on previously
+		 * @param newPage The page the view has moved to
+		 */
+		public abstract void onPageChanged(int oldPage, int newPage);
+	}
+	
+	/**
+	 * Set the current OnPageChangedListsner
+	 * 
+	 * @param onPageChangedListener The OnPageChangedListener object
+	 */
+	public void setOnPageChangedListener(OnPageChangedListener onPageChangedListener)
+	{
+		mOnPageChangedListener = onPageChangedListener;
+	}
+	
+	/**
+	 * Get the current OnPageChangeListsner
+	 * 
+	 * @return The current OnPageChangedListener
+	 */
+	public OnPageChangedListener getOnPageChangedListener()
+	{
+		return mOnPageChangedListener;
+	}
+	
+	private class SwipeOnTouchListener implements View.OnTouchListener
+	{
+
+		public boolean onTouch(View v, MotionEvent event) 
+		{
+			switch(event.getAction())
+			{
+				case MotionEvent.ACTION_DOWN :
+					motionStartX = (int) event.getX();
+					firstMotionEvent = false;
+					return false;
+					
+				case MotionEvent.ACTION_MOVE :
+					
+					int newDistance = motionStartX - (int) event.getX();
+					int newDirection;
+					
+					if(newDistance<0) //backwards
+					{
+						newDirection =  (distanceX+4 <= newDistance) ? 1 : -1;  //the distance +4 is to allow for jitter
+					}
+					else //forwards
+					{
+						newDirection =  (distanceX-4 <= newDistance) ? 1 : -1;  //the distance -4 is to allow for jitter
+					}
+					
+					
+					if(newDirection != previousDirection && !firstMotionEvent)//changed direction, so reset start point
+					{
+						motionStartX = (int) event.getX();
+						distanceX = motionStartX - (int) event.getX();
+					}
+					else
+					{
+						distanceX = newDistance;
+					}
+
+					previousDirection = newDirection; //backwards -1, forwards is 1
+					return false;
+					
+				case MotionEvent.ACTION_UP :
+					float fingerUpPosition = getScrollX();
+	                float numberOfPages = mLinearLayout.getChildCount();
+	                float pageWidth = getMeasuredWidth();
+	                float fingerUpPage = fingerUpPosition/pageWidth;
+	                float edgePosition = 0;
+	                
+	                if(previousDirection == 1) //forwards
+	                {
+	                	if(distanceX > DEFAULT_SWIPE_THRESHOLD)//if over then go forwards
+		                {
+		                	if(mCurrentPage<(numberOfPages-1))//if not at the end of the pages, you don't want to try and advance into nothing!
+		                	{
+		                		edgePosition = (int)(fingerUpPage+1)*pageWidth;
+		                	}
+		                	else
+		                	{
+		                		edgePosition = (int)(fingerUpPage)*pageWidth;
+		                	}
+		                }
+		                else //return to start position
+		                {
+		                	if(Math.round(fingerUpPage)==numberOfPages-1)//if at the end
+		                	{
+		                		//need to correct for when user starts to scroll into 
+		                		//nothing then pulls it back a bit, this becomes a 
+		                		//kind of forwards scroll instead
+		                		edgePosition = (int)(fingerUpPage+1)*pageWidth;
+		                	}
+		                	else //carry on as normal
+		                	{
+		                		edgePosition = (int)(fingerUpPage)*pageWidth;
+		                	}
+		                }
+	                	
+	                	
+	                	
+	                	
+	                }
+	                else //backwards
+	                {
+	                	if(distanceX < -DEFAULT_SWIPE_THRESHOLD)//go backwards
+		                {
+		                	edgePosition = (int)(fingerUpPage)*pageWidth;
+		                }
+		                else //return to start position
+		                {
+		                	if(Math.round(fingerUpPage)==0)//if at beginning, correct
+		                	{
+		                		//need to correct for when user starts to scroll into 
+		                		//nothing then pulls it back a bit, this becomes a 
+		                		//kind of backwards scroll instead
+		                		edgePosition = (int)(fingerUpPage)*pageWidth;
+		                	}
+		                	else //carry on as normal
+		                	{
+		                		edgePosition = (int)(fingerUpPage+1)*pageWidth;
+		                	}
+		                	
+		                }
+	                	
+	                	
+	                }
+	                
+	                smoothScrollTo((int)edgePosition, 0);
+	                firstMotionEvent = true;
+	                
+	                
+	                //fire OnPageChangedListener, talk to page control
+	                if(mCurrentPage!=(int)(edgePosition/pageWidth) && (int)(edgePosition/pageWidth)<getPageCount()) //if the page at the beginning of this action is not equal to page we are now on, i.e. if the page has changed
+	                {
+	                	//page control
+	                	if(mPageControl!=null)
+	                	{
+	                		mPageControl.setCurrentPage((int)(edgePosition/pageWidth));
+	                	}
+	               
+	                	//page changed listener 
+	                	if(mOnPageChangedListener!=null)
+	                	{
+	                		mOnPageChangedListener.onPageChanged(mCurrentPage, (int)(edgePosition/pageWidth));
+	                	}
+	                }
+	                
+	                mCurrentPage = (int)(edgePosition/pageWidth);
+	                return true;
+			}
+			
+			return false;
+		}
+		
+	}
+
+	
+}	
